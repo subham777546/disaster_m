5997c7048daab1185ee0dddc21599e8ee5c33d3f
subham777546 <subhampatnaik0111@gmail.com>
2025-03-15 20:21:30 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Update base to your repository name
-export default defineConfig({
-  plugins: [react()],
-  base: '/disaster_m/'
-})
